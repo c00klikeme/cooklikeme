3fce6f06d19b7f7624b9cee03d4a0a393b2f6788
c00klikeme <cookjustlikeme@gmail.com>
2026-08-15 20:26:16 -0400

    Create ingredients.js

---
+++ data/ingredients.js
@@ -0,0 +1,318 @@
+window.COOKLIKEME_INGREDIENTS = {
+
+  regular: {
+
+    "Proteins": [
+      "chicken",
+      "chicken thighs",
+      "fried chicken",
+      "wings",
+      "shrimp",
+      "salmon",
+      "fish",
+      "tilapia",
+      "ground beef",
+      "steak",
+      "pork chops",
+      "sausage",
+      "turkey",
+      "lamb chops",
+      "eggs",
+      "bacon"
+    ],
+
+    "Carbs & Bases": [
+      "rice",
+      "white rice",
+      "yellow rice",
+      "brown rice",
+      "pasta",
+      "spaghetti",
+      "ziti",
+      "macaroni",
+      "egg noodles",
+      "potatoes",
+      "mashed potatoes",
+      "fries",
+      "grits",
+      "beans",
+      "plantain",
+      "bread",
+      "tortilla",
+      "wrap"
+    ],
+
+    "Dairy & Rich Stuff": [
+      "butter",
+      "milk",
+      "heavy cream",
+      "cheddar",
+      "mozzarella",
+      "parmesan",
+      "american cheese",
+      "cream cheese",
+      "sour cream"
+    ],
+
+    "Veggies & Herbs": [
+      "onion",
+      "garlic",
+      "bell pepper",
+      "tomato",
+      "spinach",
+      "broccoli",
+      "cabbage",
+      "lettuce",
+      "carrots",
+      "celery",
+      "corn",
+      "green beans",
+      "mushrooms",
+      "scallion",
+      "cilantro",
+      "thyme",
+      "parsley"
+    ],
+
+    "Seasonings": [
+      "salt",
+      "black pepper",
+      "garlic powder",
+      "onion powder",
+      "paprika",
+      "smoked paprika",
+      "cajun seasoning",
+      "creole seasoning",
+      "old bay",
+      "seasoned salt",
+      "adobo",
+      "lemon pepper",
+      "italian seasoning",
+      "oregano",
+      "red pepper flakes",
+      "curry powder",
+      "jerk seasoning",
+      "thyme seasoning"
+    ],
+
+    "Sauces & Flavor": [
+      "gravy",
+      "hot sauce",
+      "buffalo sauce",
+      "bbq sauce",
+      "soy sauce",
+      "alfredo sauce",
+      "tomato sauce",
+      "marinara",
+      "garlic butter",
+      "honey garlic sauce",
+      "jerk sauce",
+      "brown stew sauce",
+      "coconut milk",
+      "lime",
+      "lemon"
+    ]
+
+  },
+
+
+  healthy: {
+
+    "Lean Proteins": [
+      "chicken breast",
+      "turkey",
+      "ground turkey",
+      "salmon",
+      "shrimp",
+      "tilapia",
+      "tuna",
+      "eggs",
+      "egg whites",
+      "greek yogurt"
+    ],
+
+    "Healthy Bases": [
+      "brown rice",
+      "quinoa",
+      "sweet potato",
+      "oats",
+      "whole wheat tortilla",
+      "whole wheat pasta",
+      "black beans",
+      "chickpeas"
+    ],
+
+    "Veggies": [
+      "broccoli",
+      "spinach",
+      "kale",
+      "bell pepper",
+      "onion",
+      "tomato",
+      "cucumber",
+      "lettuce",
+      "cabbage",
+      "green beans",
+      "carrots",
+      "mushrooms",
+      "corn",
+      "avocado"
+    ],
+
+    "Healthy Flavor": [
+      "garlic",
+      "lime",
+      "lemon",
+      "cilantro",
+      "scallion",
+      "thyme",
+      "olive oil",
+      "hot sauce",
+      "low sodium soy sauce"
+    ],
+
+    "Seasonings": [
+      "salt",
+      "black pepper",
+      "garlic powder",
+      "onion powder",
+      "paprika",
+      "smoked paprika",
+      "cajun seasoning",
+      "jerk seasoning",
+      "lemon pepper",
+      "curry powder"
+    ]
+
+  },
+
+
+  shakes: {
+
+    "Protein": [
+      "vanilla protein powder",
+      "chocolate protein powder",
+      "strawberry protein powder",
+      "greek yogurt"
+    ],
+
+    "Fruit": [
+      "banana",
+      "strawberries",
+      "blueberries",
+      "mango",
+      "pineapple",
+      "peach",
+      "mixed berries"
+    ],
+
+    "Liquid": [
+      "milk",
+      "almond milk",
+      "oat milk",
+      "water",
+      "coconut water"
+    ],
+
+    "Extras": [
+      "peanut butter",
+      "oats",
+      "honey",
+      "cinnamon",
+      "cocoa powder",
+      "coffee",
+      "ice",
+      "spinach"
+    ]
+
+  },
+
+
+  drinks: {
+
+    "Spirits 21+": [
+      "rum",
+      "white rum",
+      "dark rum",
+      "vodka",
+      "tequila",
+      "whiskey"
+    ],
+
+    "Mixers": [
+      "pineapple juice",
+      "orange juice",
+      "cranberry juice",
+      "lime juice",
+      "lemon juice",
+      "club soda",
+      "ginger beer",
+      "coconut cream",
+      "grenadine"
+    ],
+
+    "Fresh": [
+      "lime",
+      "lemon",
+      "mint",
+      "pineapple",
+      "orange",
+      "strawberries"
+    ],
+
+    "Sweeteners": [
+      "simple syrup",
+      "honey",
+      "sugar"
+    ]
+
+  },
+
+
+  desserts: {
+
+    "Baking Basics": [
+      "flour",
+      "sugar",
+      "brown sugar",
+      "eggs",
+      "butter",
+      "milk",
+      "vanilla extract",
+      "baking powder",
+      "baking soda"
+    ],
+
+    "Chocolate": [
+      "chocolate chips",
+      "cocoa powder",
+      "white chocolate"
+    ],
+
+    "Fruit": [
+      "banana",
+      "apples",
+      "peaches",
+      "strawberries",
+      "blueberries",
+      "pineapple"
+    ],
+
+    "Rich Stuff": [
+      "cream cheese",
+      "heavy cream",
+      "condensed milk",
+      "whipped cream",
+      "vanilla ice cream"
+    ],
+
+    "Flavor": [
+      "cinnamon",
+      "nutmeg",
+      "vanilla extract",
+      "caramel sauce"
+    ]
+
+  }
+
+};
